6f09bdb9aecf3a8e04e21ec3b79535dc8325be06
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-07-09 17:18:36 -0400

    add homecontainer

---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
+import AppFrame from "../components/AppFrame";
+import CustomersActions from "../components/CustomersActions";
+
+class HomeContainer extends Component {
+  handleOnClick = () => {
+    this.props.history.push("/customers");
+  };
+
+  render() {
+    return (
+      <div>
+        <AppFrame
+          header="Home"
+          body={
+            <div>
+              Esta es la pantalla inicial
+              <CustomersActions>
+                <button onClick={this.handleOnClick}>
+                  Listado de clientes
+                </button>
+              </CustomersActions>
+            </div>
+          }
+        />
+      </div>
+    );
+  }
+}
+
+HomeContainer.propTypes = { history: PropTypes.object.isRequired };
+
+export default withRouter(HomeContainer);
