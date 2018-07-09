573aacd1d292707e38b796145dcd68cc8fd57533
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-07-09 17:19:00 -0400

    add customerListitem

---
+++ src/components/CustomerListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const CustomerListItem = ({ name, editAction, deleteAction, urlPath, dni }) => {
+  return (
+    <div className="customer-list-item">
+      <div className="field">
+        <Link to={`${urlPath}${dni}`}>{name}</Link>{" "}
+      </div>
+      <div className="field">
+        <Link to={`${urlPath}${dni}/edit`}>{editAction}</Link>{" "}
+      </div>
+      <div className="field">
+        <Link to={`${urlPath}${dni}/del`}>{deleteAction}</Link>{" "}
+      </div>
+    </div>
+  );
+};
+
+CustomerListItem.propTypes = {
+  dni: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  editAction: PropTypes.string.isRequired,
+  deleteAction: PropTypes.string.isRequired,
+  urlPath: PropTypes.string.isRequired
+};
+
+export default CustomerListItem;
