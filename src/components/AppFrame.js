0b802712355c69c09eb7854f521b2cfd03e36f34
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-07-09 17:34:02 -0400

    refactor

--- src/components/AppFrame.js
+++ src/components/AppFrame.js
@@ -8,7 +8,9 @@ const AppFrame = ({ header, body }) => {
       <div className="app-frame">
         <AppHeader title={header} />
         <div>{body}</div>
-        <div>Aplicación Simple de ejemplo</div>
+        <div>
+          <p>Aplicación Simple de Ejemplo</p>
+        </div>
       </div>
     </div>
   );
